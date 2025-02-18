fa429f370452e5841282e27b8731cea48b78a157
Lucas Hom <lucasrylanhom@gmail.com>
2025-02-18 14:23:07 -0800

    Created PurchaseProperties abstract class

---
+++ Assets/Scripts/Abstract/PuchaseProperties.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class PuchaseProperties : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    //Classes
+    [SerializeField] protected FloatingUpgradeText FloatingText;
+    [SerializeField] protected ShopManager ShopManager;
+
+    //Prefabs
+    [SerializeField] protected GameObject notEnoughTextPrefab;
+    [SerializeField] protected GameObject maxedUpgradeTextPrefab;
+    [SerializeField] protected GameObject upgradeCostTextPrefab;
+
+    //Child objects
+    [SerializeField] protected GameObject PriceObject;
+    [SerializeField] protected TextMeshProUGUI StatusText;
+    protected TextMeshProUGUI PriceText;
+
+
+    [SerializeField] protected int basePrice = 4;
+
+
+    protected List<GameObject> instantiatedObjects = new List<GameObject>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PriceText = PriceObject.GetComponent<TextMeshProUGUI>();
+        PriceText.text = $"${basePrice}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public abstract void AttemptPurchase(Action purchaseAction);
+
+    public virtual void clickVisuals(int price)
+    {
+        StartCoroutine(ClickEffect());
+    }
+
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f) * 1.05f;
+    }
+
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+
+    //Get rid of prefabs on pause
+    private void OnEnable()
+    {
+        foreach (GameObject obj in instantiatedObjects)
+        {
+            Destroy(obj);
+        }
+
+        instantiatedObjects.Clear();
+    }
+
+    private IEnumerator ClickEffect()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        yield return new WaitForSecondsRealtime(0.06f);
+        transform.localScale = new Vector3(1f, 1f, 1f) * 1.05f;
+    }
+
+
+    protected void notEnough()
+    {
+        GameObject notEnoughText = Instantiate(notEnoughTextPrefab, transform);
+        instantiatedObjects.Add(notEnoughText);
+        RectTransform notEnoughTransform = maxedUpgradeTextPrefab.GetComponent<RectTransform>();
+        notEnoughTransform.position = new Vector3(0f, 0f, 0f);
+
+        StartCoroutine(FloatingText.FloatAndDeleteText(notEnoughText, 0.8f));
+    }
+
+
+    protected void maxedUpgrade()
+    {
+        GameObject maxedUpgradeText = Instantiate(maxedUpgradeTextPrefab, transform);
+        instantiatedObjects.Add(maxedUpgradeText);
+        RectTransform maxedUpgradeTransform = maxedUpgradeTextPrefab.GetComponent<RectTransform>();
+        maxedUpgradeTransform.position = new Vector3(0f, 0f, 0f);
+
+        StartCoroutine(FloatingText.FloatAndDeleteText(maxedUpgradeText, 0.8f));
+    }
+
+    //For rank upgrades
+    protected void removeOldPrice(int price)
+    {
+        ShopManager.currency -= price;
+        ShopManager.updateCurrency();
+
+        GameObject oldUpgradeCostText = Instantiate(upgradeCostTextPrefab, transform);
+        instantiatedObjects.Add(oldUpgradeCostText);
+        RectTransform oldCostRectTransform = oldUpgradeCostText.GetComponent<RectTransform>();
+        oldCostRectTransform.position = PriceObject.GetComponent<RectTransform>().position;
+
+        oldUpgradeCostText.GetComponent<TextMeshProUGUI>().text = $"${price}";
+
+        StartCoroutine(FloatingText.FloatAndDeleteText(oldUpgradeCostText, 0.4f));
+    }
+}
--- Assets/Scripts/RankUpgrades.cs
+++ Assets/Scripts/RankUpgrades.cs
@@ -1,28 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class RankUpgrades : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class RankUpgrades : PuchaseProperties
 {
     //Classes
     [SerializeField] private GenerateWater WaterGenerator;
-    [SerializeField] private ShopManager ShopManager;
-    [SerializeField] private FloatingUpgradeText FloatingText;
     [SerializeField] private Player Player;
 
-    //Objects
-    [SerializeField] private GameObject PriceObject;
-    [SerializeField] private TextMeshProUGUI RankText;
-    private TextMeshProUGUI PriceText;
-    [SerializeField] private GameObject upgradeCostTextPrefab;
-    [SerializeField] private GameObject maxedUpgradeTextPrefab;
-    [SerializeField] private GameObject notEnoughTextPrefab;
-
-    //Prices
-    [SerializeField] private int upgradeBasePrice = 4;
 
     //MaxUpgrades
     [SerializeField] private int maxAmountUpgrades = 20;
@@ -33,16 +23,10 @@ public class RankUpgrades : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     //Scalars
     private float upgradeMultiplier = 1.15f;
 
-    private List<GameObject> instantiatedObjects = new List<GameObject>();
-
-
     // Start is called before the first frame update
     void Start()
     {
-        PriceText = PriceObject.GetComponent<TextMeshProUGUI>();
-
-        RankText.text = $"Rank {upgradeRank}";
-        PriceText.text = $"${upgradeBasePrice}";
+        StatusText.text = $"Rank {upgradeRank}";
     }
 
     // Update is called once per frame
@@ -51,17 +35,8 @@ public class RankUpgrades : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         
     }
 
-    private void OnEnable()
-    {
-        foreach (GameObject obj in instantiatedObjects)
-        {
-            Destroy(obj);
-        }
-
-        instantiatedObjects.Clear();
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
+    //Rankupgrade override - only increase size if not max upgraded
+    public override void OnPointerEnter(PointerEventData eventData)
     {
         if (upgradeRank < maxAmountUpgrades)
         {
@@ -69,63 +44,17 @@ public class RankUpgrades : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         }
     }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        transform.localScale = new Vector3(1f, 1f, 1f);
-    }
-
-    private void notEnough()
-    {
-        GameObject notEnoughText = Instantiate(notEnoughTextPrefab, transform);
-        instantiatedObjects.Add(notEnoughText);
-        RectTransform notEnoughTransform = maxedUpgradeTextPrefab.GetComponent<RectTransform>();
-        notEnoughTransform.position = new Vector3(0f, 0f, 0f);
-
-        StartCoroutine(FloatingText.FloatAndDeleteText(notEnoughText, 0.8f));
-    }
-    
-    private void maxedUpgrade()
-    {
-        GameObject maxedUpgradeText = Instantiate(maxedUpgradeTextPrefab, transform);
-        instantiatedObjects.Add(maxedUpgradeText);
-        RectTransform maxedUpgradeTransform = maxedUpgradeTextPrefab.GetComponent<RectTransform>();
-        maxedUpgradeTransform.position = new Vector3(0f, 0f, 0f);
-
-        StartCoroutine(FloatingText.FloatAndDeleteText(maxedUpgradeText, 0.8f));
-    }
-
-    private IEnumerator ClickEffect()
-    {
-        transform.localScale = new Vector3(1f, 1f, 1f);
-        yield return new WaitForSecondsRealtime(0.06f);
-        transform.localScale = new Vector3(1f, 1f, 1f) * 1.05f;
-    }
-
-    private void removeOldPrice(int price)
-    {
-        ShopManager.currency -= price;
-        ShopManager.updateCurrency();
-
-        GameObject oldUpgradeCostText = Instantiate(upgradeCostTextPrefab, transform);
-        instantiatedObjects.Add(oldUpgradeCostText);
-        RectTransform oldCostRectTransform = oldUpgradeCostText.GetComponent<RectTransform>();
-        oldCostRectTransform.position = PriceObject.GetComponent<RectTransform>().position;
-
-        oldUpgradeCostText.GetComponent<TextMeshProUGUI>().text = $"${price}";
-        
-        StartCoroutine(FloatingText.FloatAndDeleteText(oldUpgradeCostText, 0.4f));
-    }
-
-    private void clickVisuals(int price)
+    //Rankupgrade override - remove prices
+    public override void clickVisuals(int price)
     {
         removeOldPrice(price);
-        StartCoroutine(ClickEffect());  
-        //Activate particle system
+        base.clickVisuals(price);
     }
 
-    public void upgradeWaterCapacity()
+    //Override for rank upgrade purchase
+    public override void AttemptPurchase(Action upgradeAction)
     {
-        int price = (int)Mathf.Ceil(upgradeBasePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
+        int price = (int)Mathf.Ceil(basePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
 
         if (upgradeRank < maxAmountUpgrades)
         {
@@ -134,12 +63,12 @@ public class RankUpgrades : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
                 clickVisuals(price);
 
                 upgradeRank += 1;
-                RankText.text = $"Rank {upgradeRank}";
-                int nextPrice = (int)Mathf.Ceil(upgradeBasePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
+                StatusText.text = $"Rank {upgradeRank}";
+                int nextPrice = (int)Mathf.Ceil(basePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
 
                 PriceText.text = upgradeRank >= maxAmountUpgrades ? "Max" : $"${nextPrice}";
 
-                WaterGenerator.maxWater += 1;
+                upgradeAction();
             }
             else
             {
@@ -150,67 +79,28 @@ public class RankUpgrades : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
         {
             maxedUpgrade();
         }
+    }
+
+
+    //Specific purchases
+    public void upgradeWaterCapacity()
+    {
+        AttemptPurchase(() => { WaterGenerator.maxWater += 1; });
     }
 
     public void upgradeReloadSpeed()
     {
-        int price = (int)Mathf.Ceil(upgradeBasePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
-
-        if (upgradeRank < maxAmountUpgrades)
-        {
-            if (ShopManager.currency > price)
-            {
-                clickVisuals(price);
-
-                upgradeRank += 1;
-                RankText.text = $"Rank {upgradeRank}";
-                int nextPrice = (int)Mathf.Ceil(upgradeBasePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
-
-                PriceText.text = upgradeRank >= maxAmountUpgrades ? "Max" : $"${nextPrice}";
-
-                WaterGenerator.reloadDelay -= 0.008f;
-            }
-            else
-            {
-                notEnough();
-            }
-        }
-        else
-        {
-            maxedUpgrade();
-        }
+        AttemptPurchase(() => { WaterGenerator.reloadDelay -= 0.008f; });
     }
 
 
     public void upgradePlayerSpeed()
     {
-        int price = (int)Mathf.Ceil(upgradeBasePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
-
-        if (upgradeRank < maxAmountUpgrades)
-        {
-            if (ShopManager.currency > price)
-            {
-                clickVisuals(price);
-
-                upgradeRank += 1;
-                RankText.text = $"Rank {upgradeRank}";
-                int nextPrice = (int)Mathf.Ceil(upgradeBasePrice * Mathf.Pow(upgradeMultiplier, upgradeRank));
-
-                PriceText.text = upgradeRank >= maxAmountUpgrades ? "Max" : $"${nextPrice}";
-
-                Player.playerSpeed += 0.18f;
-                Player.accelerationRate += 0.12f;
-            }
-            else
-            {
-                notEnough();
-            }
-        }
-        else
-        {
-            maxedUpgrade();
-        }
+        AttemptPurchase(() => {
+            Player.playerSpeed += 0.18f;
+            Player.accelerationRate += 0.12f;
+        });
     }
 
-    //Mkae function to create frame for citizen and player upgrades
+    
 }
